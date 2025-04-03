eb10ece4a9d9fcb76ddbc337c77925f577300426
meijo <Junki Kawano>
2025-04-04 02:24:10 +0900

    右クリックでブックマークできるように

--- extension/test/test.py
+++ extension/test/test.py
@@ -3,16 +3,30 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/api/histories', methods=['POST'])
-def nya():
+def histories():
     data = request.get_json()
 
     print(data)
 
     response = {
-        status: 200,
-        message: "add history successful"
+        "status": 200,
+        "message": "add history successful"
     }
 
+    return jsonify(response)
+
+
+@app.route('/api/bookmarks/default', methods=['POST'])
+def bookmarks_default():
+    data = request.get_json()
+
+    print(data)
+
+    response = {
+        "status": 200,
+        "message": "bookmark successful",
+        "id": 123456790
+    }
     return jsonify(response)
 
 
