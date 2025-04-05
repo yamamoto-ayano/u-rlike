7d8b4d1e50ed206a56e548125b477542de0476a5
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-04-05 14:18:02 +0900

    全体で使えるようにした

--- frontend/src/components/common/clientWrapper.tsx
+++ frontend/src/components/common/clientWrapper.tsx
@@ -2,8 +2,15 @@
 "use client";
 
 import { ReactNode } from "react";
+import { ModalProvider } from "../folder/FolderFormContext";
 import { SidebarProvider } from "@/components/common/SidebarContext";
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
-  return <SidebarProvider>{children}</SidebarProvider>;
+  return (
+  <SidebarProvider>
+    <ModalProvider>
+      {children}
+    </ModalProvider>
+  </SidebarProvider>
+  )
 }
